3344e695489cbf436ff443c1b4c552683ce6d867
Facundo-github <facundo.contreras92@gmail.com>
2026-04-23 16:05:06 -0300

    desarrollo sobre run.py

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,10 @@
 from app import create_app
+from app.database import db
+from flask_migrate import Migrate
 
 app = create_app()
+migrate= Migrate(app, db)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
